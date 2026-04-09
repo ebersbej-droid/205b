966006a1e865b0b081fcb8d458da762841167b8d
ravir <ebersbej@uci.edu>
2026-04-09 09:41:05 +0000

    added filtered data

--- repo/scripts/fetch-csvs.sh
+++ repo/scripts/fetch-csvs.sh
@@ -10,24 +10,25 @@ TMP_DIR=$(mktemp -d)
 data=$(date +%Y-%m-%d)
 
 # Ran native python3 in line since 
-# 1. wasn't sure I could add curl/wget etc. to docker file
+# 1. wasn't sure I could add curl/wget etc. to docker file (I figured we need unzip for obvious reasons)
 # 2. can somehow make a seperate .py file to run this command/code
 
 python3 -c "import urllib.request; url = 'https://raw.githubusercontent.com/joachimvandekerckhove/cogs205b-s26/9dca64e57fd88213f2422c19a8b10953a8fbfdbe/modules/02-version-control/files/data.zip'; urllib.request.urlretrieve(url,'$TMP_DIR/local_file.zip')"
 
-unzip -q "$TMP_DIR/local_file" -d "$TMP_DIR/unzipped"
+unzip -q "$TMP_DIR/local_file.zip" -d "$TMP_DIR/unzipped"
 
 # Create filtered file directory
-
 mkdir -p ../data/$data 
-#cd ../data
 
-find "$TMP_DIR/unzipped" -name "*.csv" -exec mv -t ../data/$data {} +
+# Searching for files
 
-echo "File filtered & folder '$data' created."
+find "$TMP_DIR/unzipped" -maxdepth 1 -name "*.csv" -exec mv -t ../data/$data {} +
 
+echo "Beep Boop Bop Git Hub stuff"
 
 git -C .. add data/ scripts/
 git -C .. commit -m "added filtered data"
 git -C .. pull --rebase origin main 
 git -C .. push origin main
+
+echo "File filtered & folder '$data' created."
